dbefef1fedf1bac80967561f41e0b52d625b5dff
Diego <diego.lavayen@digitalbridge.com>
2021-07-08 19:00:04 +0100

    feat: add general App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import WordList from './components/WordList';
+import { useAuth } from './contexts/AuthContext';
 
-function App() {
+const App = () => {
+  const [option, setOption] = useState();
+  const { currentUser, logout, loginWithGoogle } = useAuth();
+
+  console.log(currentUser);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {!currentUser && !option && (
+        <>
+          <button type="button" onClick={() => setOption('login')}>
+            Login
+          </button>
+          <button type="button" onClick={() => setOption('signup')}>
+            Signup
+          </button>
+          <button type="button" onClick={loginWithGoogle}>
+            Login With Google
+          </button>
+        </>
+      )}
+      {!currentUser && option && (
+        <>{option === 'login' ? <Login /> : <Signup />}</>
+      )}
+      {currentUser && (
+        <button type="button" onClick={logout}>
+          Logout
+        </button>
+      )}
+      <WordList />
+    </>
   );
-}
+};
 
 export default App;
